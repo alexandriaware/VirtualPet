3da67a1c2ec2d1dbb518b952c76219d309b919f8
alexandriaware <ali.mercer92@gmail.com>
2017-11-27 10:14:16 -0500

    project commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualPet elsie = new VirtualPet("Elsie", 1, "Grey");
+            string selection;
+
+            Console.WriteLine("Congratulations on your new pet elephant! You've decided to name her Elsie.");
+            ElsieArt();
+            Console.WriteLine(elsie.Stats());
+            Console.WriteLine("\n");
+            Pause();
+
+            do
+            {
+                Console.WriteLine("What would you like to do with Elsie?");
+                Menu();
+                selection = Console.ReadLine().Trim();
+
+                if (selection == "1")
+                {
+                    elsie.Eat();
+                    Pause();
+                }
+                else if (selection == "2")
+                {
+                    elsie.Drink();
+                    Pause();
+                }
+                else if (selection == "3")
+                {
+                    elsie.Play();
+                    Pause();
+                }
+                else if (selection == "4")
+                {
+                    elsie.Sleep();
+                    Pause();
+                }
+                else if (selection == "5")
+                {
+                    Console.WriteLine("Elsie looks sad and goes to play by herself.");
+                    Pause();
+                }
+                else
+                {
+                    if (selection != "6")
+                    {
+                        Console.WriteLine("Please choose something to do with Elsie!");
+                    }
+                    
+                }
+
+            }
+            while (selection != "6");
+
+            {
+                Console.WriteLine("Thanks for playing!");
+                Environment.Exit(0);
+            }
+
+        }
+
+
+
+
+
+        //Methods
+        static void Menu()
+        {
+
+            Console.WriteLine("1. Feed Elsie");
+            Console.WriteLine("2. Water Elsie");
+            Console.WriteLine("3. Play with Elsie");
+            Console.WriteLine("4. Time for Elsie's nap!");
+            Console.WriteLine("5. Do nothing");
+            Console.WriteLine("6. Exit");
+        }
+
+        static void ElsieArt()
+        {
+            Console.WriteLine(@"
+
+                               /  \~~~/  \    
+                         ,----(     ..    ) 
+                        /      \__     __/   
+                       /|         (\  |(
+                      ^ \   /___\  /\ |   
+                         |__|   |__|    ");
+        }
+
+        static void Pause()
+        {
+            System.Threading.Thread.Sleep(2500);
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //STATES
+        //fields
+        protected string petName;
+        protected string furColor;
+        protected int petAge;
+        public int hunger;
+        public int thirst;
+        public int boredom;
+        public int exhaustion;
+        protected bool isSick = false;
+
+        //Properties
+        public string PetName
+        {
+            get { return this.petName; }
+            set { this.petName = value; }
+        }
+
+        public string FurColor { get; set; }
+        public int PetAge { get; set; }
+
+        //BEHAVIORS
+        //Constructors
+        public VirtualPet()
+        {
+
+        }
+        public VirtualPet(string petName, int petAge, string furColor)
+        {
+            this.petName = petName;
+            this.petAge = petAge;
+            this.furColor = furColor;
+            this.hunger = 3;
+            this.thirst = 3;
+            this.boredom = 3;
+            this.exhaustion = 1;
+        }
+
+        //Methods
+        public void Eat()
+        {
+            if (hunger <= 5 && hunger > 0)
+            {
+                hunger--;
+                Console.WriteLine("You have fed Elsie!");
+            }
+            else
+            {
+                Console.WriteLine("Elsie isn't hungry anymore!");
+            }
+        }
+        public void Drink()
+        {
+            if (thirst <= 5 && thirst > 0)
+            {
+                thirst--;
+                Console.WriteLine("You have watered Elsie!");
+            }
+            else
+            {
+                Console.WriteLine("Elsie isn't thirsty right now.");
+            }
+        }
+        public void Play()
+        {
+            if (exhaustion == 5)
+            {
+                Console.WriteLine("Elsie is too tired to play right now.");
+            }
+            else
+            {
+                boredom--;
+                hunger++;
+                thirst++;
+                exhaustion++;
+                Console.WriteLine("You play with Elsie and she trumpets with joy!");
+            }
+        }
+        public void Sleep()
+        {
+            if (exhaustion == 5)
+            {
+                exhaustion = 0;
+                Console.WriteLine("Elsie settles down to sleep.");
+            }
+            else
+            {
+                Console.WriteLine("Elsie doesn't want to nap! She wants to play.");
+            }
+        }
+        public string Stats()
+        {
+            return "Name: " + petName + "\nAge: " + petAge + "\nFur Color: " + furColor + "\nHunger: " + hunger + "\nThirst: " + thirst + "\nBoredom: " + boredom + "\nTiredness: " + exhaustion;
+        }
+
+
+    }
+}
